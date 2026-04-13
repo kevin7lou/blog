44b216a1ac2c596cb936c47c620fa9227158bdaa
Kevin Lou <1493360+kevin7lou@users.noreply.github.com>
2026-04-13 22:20:11 +0800

    feat: add Zhihu and Jike social icons

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,11 @@
 import type { Props } from "astro";
-import IconMail from "@/assets/icons/IconMail.svg";
 import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
-import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
-import IconFacebook from "@/assets/icons/IconFacebook.svg";
+import IconZhihu from "@/assets/icons/IconZhihu.svg";
+import IconJike from "@/assets/icons/IconJike.svg";
+import IconRss from "@/assets/icons/IconRss.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
-import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import IconMail from "@/assets/icons/IconMail.svg";
 import { SITE } from "@/config";
 
 interface Social {
@@ -19,43 +18,37 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "GitHub",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/kevin7lou",
     linkTitle: `${SITE.title} on GitHub`,
     icon: IconGitHub,
   },
   {
-    name: "X",
-    href: "https://x.com/username",
-    linkTitle: `${SITE.title} on X`,
+    name: "Twitter",
+    href: "https://twitter.com/kevin7lou",
+    linkTitle: `${SITE.title} on Twitter`,
     icon: IconBrandX,
   },
   {
-    name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
-    linkTitle: `${SITE.title} on LinkedIn`,
-    icon: IconLinkedin,
+    name: "知乎",
+    href: "https://www.zhihu.com/people/kevin7lou",
+    linkTitle: `${SITE.title} on 知乎`,
+    icon: IconZhihu,
   },
   {
-    name: "Mail",
-    href: "mailto:yourmail@gmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
-    icon: IconMail,
+    name: "即刻",
+    href: "https://okjk.co/sCWqF2",
+    linkTitle: `${SITE.title} on 即刻`,
+    icon: IconJike,
+  },
+  {
+    name: "RSS",
+    href: "/rss.xml",
+    linkTitle: `${SITE.title} RSS Feed`,
+    icon: IconRss,
   },
 ] as const;
 
 export const SHARE_LINKS: Social[] = [
-  {
-    name: "WhatsApp",
-    href: "https://wa.me/?text=",
-    linkTitle: `Share this post via WhatsApp`,
-    icon: IconWhatsapp,
-  },
-  {
-    name: "Facebook",
-    href: "https://www.facebook.com/sharer.php?u=",
-    linkTitle: `Share this post on Facebook`,
-    icon: IconFacebook,
-  },
   {
     name: "X",
     href: "https://x.com/intent/post?url=",
@@ -67,12 +60,6 @@ export const SHARE_LINKS: Social[] = [
     href: "https://t.me/share/url?url=",
     linkTitle: `Share this post via Telegram`,
     icon: IconTelegram,
-  },
-  {
-    name: "Pinterest",
-    href: "https://pinterest.com/pin/create/button/?url=",
-    linkTitle: `Share this post on Pinterest`,
-    icon: IconPinterest,
   },
   {
     name: "Mail",
